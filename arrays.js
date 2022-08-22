8c881770bc35df8d9d8ae9f4ffc8f8d8c2d5dab9
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2022-08-22 07:50:36 -0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,1 +1,14 @@
+const valores = [7.7, 8.9, 6.3, 9.2] // array
+console.log(valores[0], valores[3])
+console.log(valores[4])
 
+valores[4] = 10 // adcionando elemento no array
+console.log(valores) 
+console.log(valores.length) // informa quantos elementos tem no array
+
+valores.push(100) // adicionando elemento
+console.log(valores)
+
+console.log(valores.pop()) // retirando ultimo elemento do array
+delete valores [0] // Apagando item no indice 0
+console.log(valores)
