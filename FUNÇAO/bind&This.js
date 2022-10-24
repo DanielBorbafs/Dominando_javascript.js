45fa618f1be84e44dcf8b9869bb0cc21d572882d
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2022-10-24 11:04:07 -0300

    Update bind&This.js

--- FUNÇAO/bind&This.js
+++ FUNÇAO/bind&This.js
@@ -11,3 +11,15 @@ falar()
 
 const falardepessoa = pessoa.falar.bind(pessoa)
 falardePessoa()
+
+
+function Pessoa() {
+    this.idade = 0
+    
+    const self = this
+    setInterval(function() {
+        this.idade++
+        console.log(this.idade)
+    } /*.bind(this)*/, 1000)
+
+}
