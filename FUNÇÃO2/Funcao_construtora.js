8493ede4ee1fb8c496395da4ccf1fb54f49678dc
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2022-10-26 12:15:06 -0300

    Update Funcao_construtora.js

--- FUNÇÃO2/Funcao_construtora.js
+++ FUNÇÃO2/Funcao_construtora.js
@@ -30,3 +30,25 @@ function Carro(velocidadeMaxima = 200, delta = 5) {
   
   
 }
+
+class Pessoa {
+   constructor(nome) {
+        this.nome = nome
+   }
+   falar() {
+      console.log('Meu nome é ${this.nome}')
+  }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+
+const criarPessoa = nome => {
+      return {
+        falar: () => console.log('Meu nome é ${nome}')
+      }
+}
+
+const p2 = criarPessoa('joão')
+
+
