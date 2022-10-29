6de0d2d7f7cca0b08a19b57d7f34710a69057841
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2022-10-29 08:00:51 -0300

    Create notacaoliteral.js

---
+++ OBJETO/notacaoliteral.js
@@ -0,0 +1,28 @@
+const a = 1
+const b = 2
+const c = 3
+
+const objt1 = { a: a, b: b, c: c}
+const objt2 = {a, b , c }
+console.log(obj1 , obj2)
+
+const nomeAttr = 'nota'
+const valorAttr = 7.87
+
+cohnst Obj3 = {}
+obj3[nomeAttr] = valorAttr
+console.log(obj3)
+
+const obj4 = {[nomeAttr] : valorAttr}
+
+const obj5 = {
+    funcao1: function() {
+      //.... 
+    
+    },
+    funcao2() {
+       //....
+    }
+}
+
+console.log(obj5)
