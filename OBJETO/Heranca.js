97e8bb75b656b9dc47864aa8164415dc3385e1f3
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2022-11-03 08:54:13 -0300

    Update Heranca.js

--- OBJETO/Heranca.js
+++ OBJETO/Heranca.js
@@ -66,3 +66,51 @@ console.log(ferrari.status())
 
 volvo.acelerarMais(100)
 console.log(volvo.status())
+//////////////////////////////////////////////////
+console.log(typeof String)
+console.log(typeof Array)
+console.log(typeof Object)
+
+String.prototype.reverse = function () {
+    return this.split('')
+}
+
+console.log('Escola Cod3r'.reverse())
+
+Array.prototype.first = function() {
+    return this [0]
+
+}
+
+console.log([1, 2, 3, 4, 5].first())
+console.log(['a', 'b','c'].first())
+
+String.prototype.toString = function() {
+    return 'Lascou Tudo'
+}
+
+console.log('Escola Cod3r'.reverse())
+
+function Aula(nome, videoID) {
+    this.nome = nome
+    this.videoID = videoID
+}
+
+const aula1 = new Aula('Bem vindo', 123)
+const aula2 = new Aula('Até Breve', 456)
+console.log(aula1, aula2)
+
+// Simulando o New
+
+function novo(f, ...params) {
+
+    const obj = {}
+    obj.___proto__ = f.prototype
+    f.apply(obj, params)
+    return obj
+
+}
+
+const aula3 = novo(Aula, 'BemVindo', 123)
+const aula4 = novo(Aula, 'Até breve', 123)
+console.log(aula3, aula4)
